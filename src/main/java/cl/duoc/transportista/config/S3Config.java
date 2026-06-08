1348918b67ecd69c211e5b6400a2af0efd86cb73
Emilia Acevedo <emiliaacevedosteinmetz@gmail.com>
2026-06-08 15:58:50 -0400

    Se usa AwsSessionCredentials para lab AWS

--- src/main/java/cl/duoc/transportista/config/S3Config.java
+++ src/main/java/cl/duoc/transportista/config/S3Config.java
@@ -3,7 +3,7 @@ package cl.duoc.transportista.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -19,9 +19,10 @@ public class S3Config {
         return S3Client.builder()
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(
+                        AwsSessionCredentials.create(
                                 System.getenv("AWS_ACCESS_KEY_ID"),
-                                System.getenv("AWS_SECRET_ACCESS_KEY")
+                                System.getenv("AWS_SECRET_ACCESS_KEY"),
+                                System.getenv("AWS_SESSION_TOKEN")
                         )
                 ))
                 .build();
